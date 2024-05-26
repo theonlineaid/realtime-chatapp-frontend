cdb9f6d693f844c92fc901ce3703c6f1dd142c4d
theonlineaid <nkomorfaruk@gmail.com>
2024-05-26 14:24:19 +0600

    Change frontend port

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
 
   server: {
-    port: 3000,
+    port: 7000,
     // proxy: {
     //   "/api": {
     //     target: "http://localhost:5000/api/v1",
